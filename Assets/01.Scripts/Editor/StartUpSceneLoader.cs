a53bc860954ea0e7dcbe10df92ff92de9c4abf3a
PSB08 <Parkseongbin08@gmail.com>
2024-09-09 23:56:25 +0900

    StartUpSceneLoader Script

---
+++ Assets/01.Scripts/Editor/StartUpSceneLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class StartUpSceneLoader
+{
+    static StartUpSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadStartupScene;
+    }
+
+    private static void LoadStartupScene(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            if (EditorSceneManager.GetActiveScene().buildIndex != 0)
+            {
+                EditorSceneManager.LoadScene(0);
+            }
+        }
+    }
+
+}
